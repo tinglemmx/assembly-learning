e0a9fc8a4aedb73e6460b46a9ab72b7007dbc1e3
Jiaqi Hu <tingle.mmx@hotmail.com>
2021-09-27 16:26:48 +0800

    new 10

---
+++ 汇编语言（第三版）/Chapter 10 Experiment 10.asm
@@ -0,0 +1,49 @@
+assume cs:code
+data segment
+    db 'Welcome to masm!',0
+data ends
+
+code segment
+start:  mov dh,8
+        mov dl,3
+        mov cl,2
+        mov ax,data
+        mov ds,ax
+        mov si,0
+        call show_str
+
+        mov ax,4c00h
+        int 21h
+show_str:   push dx
+            push cx
+            mov ax,0b800h
+            mov es,ax
+            mov al,dh
+            sub al,1
+            mov bl,160
+            mul bl
+            mov dh,0
+            add ax,dx
+            sub ax,1
+            mov di,ax
+            mov al,dl
+            sub al,1
+            mov bl,2
+            mul bl
+            add di,ax
+            mov al,cl
+
+change:     mov cl,[si]
+            mov ch,0
+            jcxz ok
+            mov es:[di],cx
+            inc di
+            mov es:[di],al
+            inc di
+            inc si
+            jmp short change
+ok:         pop cx
+            pop dx            
+            ret
+code ends
+end start
