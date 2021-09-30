2e21501d79488c24a0be80472a6a76604ca9583f
Jiaqi Hu <tingle.mmx@hotmail.com>
2021-09-30 17:53:13 +0800

    new

--- 汇编语言（第三版）/Chapter 10 Experiment 10.2.asm
+++ 汇编语言（第三版）/Chapter 10 Experiment 10.2.asm
@@ -1,21 +1,31 @@
 assume cs:codesg
-datasg segment
-    dd 0,0,0,0,0,0
-datasg ends
-
-; ds[0] = 结果dx ，结果的高16位
-; ds[2] = 结果ax ，结果的低16位
-; ds[4] = 结果cx , 余数
 codesg segment
-start:  mov ax,datasg
-        mov ds,ax
-        mov bx,0
-        mov ax,4240h
+start:  mov ax,4240h
         mov dx,000fh
         mov cx,0ah
         call divdw
         mov ax,4c00h
         int 21h
+
+
+
+divdw:  push dx
+        push cx
+        push bx
+        push ax
+        mov ax,dx
+        mov dx,0
+        div cx
+        mov bx,ax
+        pop ax
+        div cx
+        mov cx,dx
+        mov dx,bx
+        pop bx
+        ret
+
+codesg ends
+end start
 
 ;说明：进行不会溢出的除数运算   被除数 dword   除数 word 结果 dword
 ;参数：  ax dword 型数据低16位
@@ -24,24 +34,10 @@ start:  mov ax,datasg
 ;结果： 返回 dx = 结果的高16位  0001h
 ;           ax = 结果的低16位  86a0h
 ;           cx = 余数          0
-divdw:  push cx
-        push dx
-        push ax
-        mov ax,dx
-        mov dx,0
-        div cx
-        pop ax
-        push ax
-        div cx
-        mov word ptr ds:[0h],ax
-        mov word ptr ds:[4h],dx
-        pop ax
-        pop dx
-        pop cx
-        mov ax,dx
-        mov dx,0
-        div cx
-        mov word ptr ds:[2h],ax
-        ret
-codesg ends
-end start
+; 其实算法是  x/n = int（h/n）*65535+[rem(h/n)*65535+l]/n
+; int() 商
+; rem() 余数
+; x 被除数
+; n 除数
+; h x的高16位
+; l x的低16位
