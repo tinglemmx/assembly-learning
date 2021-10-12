5ab5841cfae8acb5ee81b3ed7b463ddb93f9e8c6
Jiaqi Hu <tingle.mmx@hotmail.com>
2021-10-13 00:33:14 +0800

    优化算法用压栈 替代之前的算法

--- 汇编语言（第三版）/Chapter 10 Experiment 10.3.asm
+++ 汇编语言（第三版）/Chapter 10 Experiment 10.3.asm
@@ -1,137 +1,169 @@
 ;将data段中的数据以十进制的形式显示出来
-assume cs:codesg,ss:stack
+assume cs:codesg
 data segment
-    dw 123,12666,1,8,3,38
+    dd 1945,1946
+    dd 16,100
+    dd 3,3
+    dd 8,20
 data ends
+
 data1 segment
-    dw 13 dup(0)
+    db 2,4,1,1,2,24
 data1 ends
+;0=总共行 
+;1=总共列
+;2=当前的行 
+;3=当前列
+;4=颜色
+;5=行宽
+
+
 stack segment
-	dw 13 dup(0)
+	db 32 dup(0)
 stack ends
 
+
 codesg segment
-start:  mov ax,stack
+start:  mov ah,0fh
+        int 10h
+        mov ah,0h
+        int 10h
+        mov ax,data1
+        mov es,ax
+        mov ax,stack 
         mov ss,ax
         mov ax,data
-        mov es,ax
-        mov ax,data1
         mov ds,ax
-        mov di,0
-        mov si,0
-        mov cx,6
-s:      mov ax,es:[di]
-        call dtoc
-        add di,2
-        loop s
-        sub bx,1
-        mov [bx],0
+        mov si,2
+        mov cx,2
+s2:     mov al,es:[si]
+        sub al,1
+        mov es:[si],al
+        inc si
+        loop s2
 
-        mov dh,20
-        mov dl,3
-        mov cl,2
-        call show_str
-        mov ax,4c00h
+        mov ax,0
+        mov bx,0
+        mov cx,0
+        mov dx,0
+
+s0:     mov cl,es:[2]
+        mov ch,es:[3]
+        call dtoc
+        mov ch,es:[3]
+        inc ch
+        mov ah,es:[1]
+        mov es:[3],ch
+        sub ah,ch
+        mov ch,0
+        mov cl,ah
+        jcxz s1
+        jmp s0
+s1:     mov cl,es:[2]
+        inc cl
+        mov es:[2],cl
+        mov byte ptr es:[3],0
+        mov al,es:[0]
+        sub al,cl
+        mov ch,0
+        mov cl,al
+        jcxz s_ok
+        jmp s0
+s_ok:   mov ax,4c00h
         int 21h    
 
 ; dtoc说明：将word型数据变为表示十进制数的字符串，字符串以0为结尾符。
-; 参数：ax = word型数据
+; 参数：es;di指向要转的首地址
 ; 参数：ds:si指向字符串的首地址
-; 结果：无返回
+; 参数：通告cl=行数 ch = 列数
+; 参数：行数列数在外面循环 控制输入和输出的位置均在主程序
+; 结果：无返回，循环用行控制
 
-dtoc:       push cx
-            push dx
-            push bx
-            mov si,0
-s1:         mov cx,10
-            mov dx,0
-            div cx
-            add dx,48
-            mov [bx][si],dl
-            mov cx,ax
-            inc si
-            jcxz ok_dtoc
-            jmp s1
-
-ok_dtoc:    call order
-            pop bx
-            pop dx
-            pop cx 
-            add bx,si
-            mov [bx],","
-            inc bx       
-            ret
-
-;数字放进去的时候是反的 要换顺序
-order:      push si
+dtoc:       mov ax,0
             push ax
-            push dx
+            mov al,cl
+            mov cl,4
+            mul cl
+            mov si,ax
+            mov al,es:[3]
+            mov ah,0
+            mul cl
+            mul byte ptr es:[0]
+            mov bx,ax
+            mov ax,[bx][si]
+            mov dx,[bx].2[si]
+dtoc_s:     mov cx,10
+            call divdw
+            add cx,48
             push cx
-            push di
-            mov di,0
-            mov ax,si
-            mov dl,2
-            div dl
-            mov ch,0
-            mov cl,al
-            jcxz order_ok
-order_s0:   sub si,1
-            mov al,[bx][di]
-            mov ah,[bx][si]
-            mov [bx][si],al
-            mov [bx][di],ah
-            inc di
-            loop order_s0
-order_ok:   pop di
-            pop cx
-            pop dx
-            pop ax
-            pop si
-            ret
+            mov cx,ax
+            add cx,dx
+            jcxz show_str 
+            jmp dtoc_s        
+
+
+
 
 
 ; show_str说明：根据输入的行列和颜色 来显示字符串，字符串后面跟个0表示字符串结束
-; 参数：dh表示行
-; 参数：dl表示列
-; 参数：cl表示颜色    
-; 参数：ds表示要显示的内容的起始地址    
+; 参数：cl表示行
+; 参数：ch表示列
+; 参数：dl表示颜色     
 ; 结果：根据输入的行列和颜色 来显示字符串
 
-show_str:   push si
-            mov si,0
-            push dx
-            push cx
+show_str:   mov ah,0
+            mov al,es:[2]
+            add al,8
+            mov cl,160
+            mul cl
+            mov bx,ax
+            mov ah,0            
+            mov al,es:[3]
+            mov cl,es:[5]
+            mul cl
+            mov si,ax
             mov ax,0b800h
-            mov es,ax
-            mov al,dh
-            sub al,1
-            mov bl,160
-            mul bl
-            mov dh,0
-            add ax,dx
-            sub ax,1
-            mov di,ax
-            mov al,dl
-            sub al,1
-            mov bl,2
-            mul bl
-            add di,ax
-            mov al,cl
-
-change:     mov cl,[si]
-            mov ch,0
+            mov ds,ax
+change:     pop ax
+            mov cx,ax
+            mov ah,es:[4]
             jcxz ok_show_str
-            mov es:[di],cx
-            inc di
-            mov es:[di],al
-            inc di
-            inc si
+            mov [bx][si],ax
+            add si,2
             jmp short change
-ok_show_str:pop cx
-            pop dx 
-            pop si           
+ok_show_str:mov ax,data
+            mov ds,ax
             ret
 
+;说明：进行不会溢出的除数运算   被除数 dword   除数 word 结果 dword
+;参数：  ax dword 型数据低16位
+;        dx dword 数据的高16位
+;        cx 除数
+;结果： 返回 dx = 结果的高16位  0001h
+;           ax = 结果的低16位  86a0h
+;           cx = 余数          0
+; 其实算法是  x/n = int（h/n）*65535+[rem(h/n)*65535+l]/n
+; int() 商
+; rem() 余数
+; x 被除数
+; n 除数
+; h x的高16位
+; l x的低16位
+
+divdw:  push bx
+        push ax
+        push cx
+        mov ax,dx
+        mov dx,0
+        div cx
+        mov bx,ax
+        pop cx
+        pop ax
+        div cx
+        mov cx,dx
+        mov dx,bx
+        pop bx
+        ret
 
 codesg ends
 end start            
