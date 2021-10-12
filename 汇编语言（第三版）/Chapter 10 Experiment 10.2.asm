4e0f89505726482f5b9b25ec43b038b63f204711
Jiaqi Hu <tingle.mmx@hotmail.com>
2021-10-13 00:30:54 +0800

    优化  去掉了 多余的压栈

--- 汇编语言（第三版）/Chapter 10 Experiment 10.2.asm
+++ 汇编语言（第三版）/Chapter 10 Experiment 10.2.asm
@@ -7,11 +7,22 @@ start:  mov ax,4240h
         mov ax,4c00h
         int 21h
 
+;说明：进行不会溢出的除数运算   被除数 dword   除数 word 结果 dword
+;参数：  ax dword 型数据低16位
+;        dx dword 数据的高16位
+;        cx 除数
+;结果： 返回 dx = 结果的高16位  0001h
+;           ax = 结果的低16位  86a0h
+;           cx = 余数          0
+; 其实算法是  x/n = int（h/n）*65535+[rem(h/n)*65535+l]/n
+; int() 商
+; rem() 余数
+; x 被除数
+; n 除数
+; h x的高16位
+; l x的低16位
 
-
-divdw:  push dx
-        push cx
-        push bx
+divdw:  push bx
         push ax
         mov ax,dx
         mov dx,0
@@ -27,17 +38,3 @@ divdw:  push dx
 codesg ends
 end start
 
-;说明：进行不会溢出的除数运算   被除数 dword   除数 word 结果 dword
-;参数：  ax dword 型数据低16位
-;        dx dword 数据的高16位
-;        cx 除数
-;结果： 返回 dx = 结果的高16位  0001h
-;           ax = 结果的低16位  86a0h
-;           cx = 余数          0
-; 其实算法是  x/n = int（h/n）*65535+[rem(h/n)*65535+l]/n
-; int() 商
-; rem() 余数
-; x 被除数
-; n 除数
-; h x的高16位
-; l x的低16位
