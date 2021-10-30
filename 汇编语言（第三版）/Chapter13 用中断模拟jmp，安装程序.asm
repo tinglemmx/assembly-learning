923c72edcb6da3c1d20a14e0a2deb54e262eff71
Jiaqi Hu <tingle.mmx@hotmail.com>
2021-10-30 13:12:04 +0800

    new

---
+++ 汇编语言（第三版）/Chapter13 用中断模拟jmp，安装程序.asm
@@ -0,0 +1,33 @@
+;功能：上一个例子7ch中断的安装程序，
+;实现loop的功能
+assume cs:code
+code segment
+start:  mov ax,cs
+        mov ds,ax
+        mov si,offset jump ;设置ds:si指向原地址
+        mov ax,0
+        mov es,ax
+        mov di,200h
+        mov cx,offset jumpend-offset jump
+        cld   ;DF置为0  si 和 di 递增方向
+        rep movsb
+
+        mov ax,0   ;在7ch处写入 中断程序的入口
+        mov es,ax
+        mov word ptr es:[7ch*4],200h
+        mov word ptr es:[7ch*4+2],0
+
+        mov ax,4c00h
+        int 21h
+jump:   push bp
+        push bx
+        add [bp+2],bx
+        pop bx
+        pop bp
+        iret
+jumpend:  nop
+code ends
+end start
+
+
+
